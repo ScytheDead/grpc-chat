223ed4a31af12dad4d42f0e6bf978bcec9086ecf
Huy Nguyen <huynguyen@itrvn.com>
2021-05-20 11:09:32 +0700

    init grpc chat

--- GRPCChat/NodeJS/grpcClient.js
+++ GRPCChat/NodeJS/grpcClient.js
@@ -1,49 +1,58 @@
-var PROTO_PATH = __dirname + '/grpc_chat.proto';
-var grpc = require('grpc');
-var protoLoader = require('@grpc/proto-loader');
-var packageDefinition = protoLoader.loadSync(
-    PROTO_PATH,
-    {keepCase: true,
-     longs: String,
-     enums: String,
-     defaults: true,
-     oneofs: true
-    });
- var protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
+const PROTO_PATH = `${__dirname}/grpc_chat.proto`;
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
 
- var grpcChat = protoDescriptor.io.mark.grpc.grpcChat;
- var client = new grpcChat.ChatService('localhost:50050',
-                                        grpc.credentials.createInsecure());
-const readline = require("readline");
+const packageDefinition = protoLoader.loadSync(
+  PROTO_PATH,
+  { keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true },
+);
+const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
+
+const { grpcChat } = protoDescriptor.io.mark.grpc;
+const client = new grpcChat.ChatService('localhost:50050',
+  grpc.credentials.createInsecure());
+const readline = require('readline');
+
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
-var user = process.argv[2];
-var metadata = new grpc.Metadata();
+const user = process.argv[2];
+const metadata = new grpc.Metadata();
 metadata.add('username', user);
-var call = client.chat(metadata);
+const call = client.chat(metadata);
 
-call.on('data', function(ChatMessage) {
-    console.log(`${ChatMessage.from} ==> ${ChatMessage.message}`);
+call.on('data', ChatMessage => {
+  console.log(`${ChatMessage.from} ==> ${ChatMessage.message}`);
 });
-call.on('end', function() {
+call.on('end', () => {
   console.log('Server ended call');
 });
-call.on('error', function(e) {
-    console.log(e);
+call.on('error', e => {
+  console.log(e);
 });
 
-rl.on("line", function(line) {
-  if (line === "quit") {
+rl.on('line', line => {
+  if (line === 'quit') {
     call.end();
     rl.close();
   } else {
     call.write({
-    message : line
-  });
+      message: line,
+    });
   }
+});
+
+process.on('SIGINT', () => {
+  console.log('Caught interrupt signal');
+
+  call.end();
+  rl.close(process.exit);
 });
 
 console.log('Enter your messages below:');
--- GRPCChat/NodeJS/grpcServer.js
+++ GRPCChat/NodeJS/grpcServer.js
@@ -1,53 +1,56 @@
-var PROTO_PATH = __dirname + '/grpc_chat.proto';
-var grpc = require('grpc');
-var protoLoader = require('@grpc/proto-loader');
-var packageDefinition = protoLoader.loadSync(
-    PROTO_PATH,
-    {keepCase: true,
-     longs: String,
-     enums: String,
-     defaults: true,
-     oneofs: true
+const PROTO_PATH = `${__dirname}/grpc_chat.proto`;
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+
+const packageDefinition = protoLoader.loadSync(
+  PROTO_PATH,
+  { keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true },
+);
+const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
+
+const { grpcChat } = protoDescriptor.io.mark.grpc;
+const clients = new Map();
+
+function chat(call) {
+  call.on('data', ChatMessage => {
+    user = call.metadata.get('username');
+    msg = ChatMessage.message;
+    console.log(`${user} ==> ${msg}`);
+    for (const [msgUser, userCall] of clients) {
+      if (msgUser != user) {
+        userCall.write(
+          {
+            from: user,
+            message: msg,
+          },
+        );
+      }
+    }
+    if (clients.get(user) === undefined) {
+      clients.set(user, call);
+    }
+  });
+  call.on('end', () => {
+    const user = call.metadata.get('username');
+    clients.delete(user);
+    call.write({
+      from: 'Chat server',
+      message: 'Nice to see ya! Come back again...',
     });
- var protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
+    call.end();
+  });
+}
 
- var grpcChat = protoDescriptor.io.mark.grpc.grpcChat;
- var clients = new Map();
-
- 
- function chat(call) {
-     call.on('data', function(ChatMessage) {
-       user=call.metadata.get('username');
-       msg=ChatMessage.message;
-       console.log(`${user} ==> ${msg}`);
-       for (let [msgUser, userCall] of clients) {
-         if (msgUser != user) {
-           userCall.write(
-             {
-             from: user,
-             message : msg
-           });
-         }
-       }
-       if (clients.get(user) === undefined) {
-         clients.set(user, call);
-       }
-
-     });
-     call.on('end', function() {
-       call.write({
-         fromName: 'Chat server',
-       message : 'Nice to see ya! Come back again...'
-     });
-       call.end();
-     });
-   }
-
-
-var server = new grpc.Server();
+const server = new grpc.Server();
 server.addService(grpcChat.ChatService.service, {
-    chat:chat
+  chat,
 });
-server.bind('0.0.0.0:50050', grpc.ServerCredentials.createInsecure());
-server.start();
-console.log("gRPC Chat Server started...");
+server.bindAsync('0.0.0.0:50050', grpc.ServerCredentials.createInsecure(), () => {
+  server.start();
+});
+
+console.log('gRPC Chat Server started...');
